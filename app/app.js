05b7284fae87b1694a27cb0c0dbce8bbe3918c0c
CodingSAK <minsik0211@gmail.com>
2023-08-02 15:42:13 +0900

    fetch Login API

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 //module
 const express =  require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 //routing
@@ -11,6 +12,10 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine","ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+// url을 통해 전달되는 데이터에 한글, 공백등과 
+// 같은문자가 있을경우 인식되지 않는 문제해결
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.use("/",home);//index.js에서 router모듈 받아오기. 미들웨어등록
 
